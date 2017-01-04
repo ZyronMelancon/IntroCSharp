ba81308825252f885432ae54803eefb4a7d92ba0
ZyronMelancon <altausolaris@gmail.com>
2017-01-04 17:43:33 -0600

    Init Commit
    Recreating Zombie vs Ninja game in C#

---
+++ IntroCSharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IntroCSharp
+{
+    class Entity
+    {
+        public Entity() { }
+        public Entity(int h, int att)
+        {
+
+        }
+
+        private string name;
+        public string Name { get { return name; } }
+        private int health;
+        public int Health { get { return health; } }
+        private int attackPower;
+        public int AttackPower { get { return attackPower; } }
+
+        public virtual bool Attack(Entity a)
+        {
+            if (a.Health > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public virtual bool Defend() { return false; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
